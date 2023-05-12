45c6cd17efc7208e7b66e1671fbbf1f44a9619f2
Gary Morales <tareasdemorales@gmail.com>
2023-05-12 16:45:59 -0500

    Agregando la estructura de carpetas para trabajar la lógica de programación, las entidades, controladores, adaptadores y conexión a la base de datos

---
+++ app/src/main/java/com/example/project_kotlin/controladores/mesaControlador.kt
@@ -0,0 +1,10 @@
+package com.example.project_kotlin.controladores
+
+import com.example.project_kotlin.entidades.mesa
+
+class mesaControlador {
+    fun listado():ArrayList<mesa>{
+        var mesas = ArrayList<mesa>()
+        return mesas
+    }
+}
---
+++ app/src/main/java/com/example/project_kotlin/entidades/mesa.kt
@@ -0,0 +1,4 @@
+package com.example.project_kotlin.entidades
+
+class mesa(var codigo:Int, var cantidadAsientos:Int, var estadoMesa:String) {
+}
