01fff5c42f0775b8a23709ead7a7e160405ad3db
fabi <luisxd0812@gmail.com>
2023-05-18 19:57:59 -0500

    termine lo que me faltaba

--- app/src/main/java/com/example/project_kotlin/adaptador/adaptadores/MetodoPagoAdapter.kt
+++ app/src/main/java/com/example/project_kotlin/adaptador/adaptadores/MetodoPagoAdapter.kt
@@ -1,4 +1,21 @@
 package com.example.project_kotlin.adaptador.adaptadores
 
-class MetodoPagoAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.project_kotlin.adaptador.vistas.VistaItemMetodoPago
+import com.example.project_kotlin.entidades.MetodoPago
+
+class MetodoPagoAdapter(val info:ArrayList<MetodoPago>):RecyclerView.Adapter<VistaItemMetodoPago>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VistaItemMetodoPago {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: VistaItemMetodoPago, position: Int) {
+        TODO("Not yet implemented")
+    }
+
 }
