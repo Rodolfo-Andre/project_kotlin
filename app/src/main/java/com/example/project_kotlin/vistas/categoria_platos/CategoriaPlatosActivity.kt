d4d6f25f49bcef038fe4752057e7773fbf749a5b
holyness <afestebancieza@gmail.com>
2023-05-31 20:18:44 -0500

    CRUD categoria plato

--- app/src/main/java/com/example/project_kotlin/vistas/categoria_platos/CategoriaPlatosActivity.kt
+++ app/src/main/java/com/example/project_kotlin/vistas/categoria_platos/CategoriaPlatosActivity.kt
@@ -5,30 +5,64 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.project_kotlin.R
+import com.example.project_kotlin.adaptador.adaptadores.categoria_plato.CategoriaPlatoAdapter
+import com.example.project_kotlin.adaptador.adaptadores.mesas.ConfiguracionMesasAdapter
+import com.example.project_kotlin.dao.CategoriaPlatoDao
+import com.example.project_kotlin.db.ComandaDatabase
+import com.example.project_kotlin.entidades.CategoriaPlato
+import com.example.project_kotlin.entidades.Mesa
+import com.example.project_kotlin.utils.appConfig
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class CategoriaPlatosActivity: AppCompatActivity() {
-    private lateinit var edtBuscarCategoriaPlato: EditText
-    private lateinit var btnConsultarCategoria: Button
     private lateinit var rvCategoria: RecyclerView
     private lateinit var btnAgregarCategoria: Button
+    private lateinit var cateDao: CategoriaPlatoDao
+    private lateinit var adaptador : CategoriaPlatoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.man_categoriaplatos)
 
-        edtBuscarCategoriaPlato = findViewById(R.id.edtBuscarCategoriaPlato)
-        btnConsultarCategoria = findViewById(R.id.btnConsultarCategoria)
+
+        cateDao = ComandaDatabase.obtenerBaseDatos(appConfig.CONTEXT).categoriaPlatoDao()
         rvCategoria = findViewById(R.id.rvCategoria)
         btnAgregarCategoria = findViewById(R.id.btnAgregarCategoriaPlatos)
+        rvCategoria = findViewById(R.id.rvCategoria)
 
-        btnAgregarCategoria.setOnClickListener({ agregar() })
+        btnAgregarCategoria.setOnClickListener({agregarVista()})
+        obtenerCategorias()
     }
 
-fun agregar(){
-    var intent= Intent(this, NewCatPlatoActivity::class.java)
-    startActivity(intent)
-}
+    fun agregarVista(){
+        var intent= Intent(this, NewCatPlatoActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun obtenerCategorias() {
+        lifecycleScope.launch(Dispatchers.IO){
+            var datos = cateDao.obtenerTodo()
+            val datosCopia = ArrayList<CategoriaPlato>(datos)
+
+            withContext(Dispatchers.Main) {
+                adaptador = CategoriaPlatoAdapter(datosCopia)
+                rvCategoria.layoutManager = LinearLayoutManager(this@CategoriaPlatosActivity)
+                rvCategoria.adapter = adaptador
+            }
+
+        }
+    }
+
+
+
+
+
+
 
 }
