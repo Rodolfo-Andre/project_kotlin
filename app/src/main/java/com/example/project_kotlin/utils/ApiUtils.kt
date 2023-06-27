6c7ed0ea0841bb45047950802c387a19558cd5bc
Gary Morales <tareasdemorales@gmail.com>
2023-06-27 03:06:07 -0500

    Comanda ahora actualiza mesa a ocupado, se registra en MySql y firebase, además se ha creado un empleado con usuario y cargo global y se habilito el login

--- app/src/main/java/com/example/project_kotlin/utils/ApiUtils.kt
+++ app/src/main/java/com/example/project_kotlin/utils/ApiUtils.kt
@@ -1,14 +1,15 @@
 package com.example.project_kotlin.utils
 
-import com.example.project_kotlin.service.ApiServiceEmpleado
-import com.example.project_kotlin.service.ApiServiceEstablecimiento
-import com.example.project_kotlin.service.ApiServiceMesa
-import com.example.project_kotlin.service.ApiServicePlato
+import com.example.project_kotlin.service.*
 
 class ApiUtils {
     companion object {
         //URL con la dirección IP de cada máquina, después se coloca el de MySql subido en la nube.
         val BASE_URL="http://192.168.56.1:8091"
+
+        fun getApiServiceComanda() : ApiServiceComanda{
+            return RetrofitClient.getClient(BASE_URL).create(ApiServiceComanda::class.java)
+        }
         fun getAPIServiceMesa(): ApiServiceMesa{
             return RetrofitClient.getClient(BASE_URL).create(ApiServiceMesa::class.java)
         }
